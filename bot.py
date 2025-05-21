c85f7444dc34c0de7658ae35bf3225c01d282112
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-21 22:23:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
-# Получаем токен из переменной окружения
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+# Токен прописан напрямую
+BOT_TOKEN = "7548380199:AAF2yqncpxlTZeBekP3heA8b_N23oNGEmNw"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -13,4 +13,4 @@ async def welcome(message: types.Message):
     await message.answer("Добро пожаловать в SG Hotline!")
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
