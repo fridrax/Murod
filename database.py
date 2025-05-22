a41033e592aa1c3d31578a1ec7a75fc46110b00e
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-23 00:33:07 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,23 @@
+import asyncpg
+from config import DATABASE_URL
 
+async def init_db():
+    try:
+        conn = await asyncpg.connect(DATABASE_URL)
+        await conn.execute('''
+            CREATE TABLE IF NOT EXISTS tickets (
+                id SERIAL PRIMARY KEY,
+                user_id BIGINT,
+                lang TEXT,
+                city TEXT,
+                department TEXT,
+                message TEXT,
+                ticket_number TEXT,
+                status TEXT,
+                reply TEXT,
+                created_at TIMESTAMP DEFAULT NOW()
+            )
+        ''')
+        await conn.close()
+    except Exception as e:
+        print(f"Ошибка при подключении к базе данных: {e}")
