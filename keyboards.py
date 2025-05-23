eb234ff9ffe3f67dac57c2a76b7ad11f1e873b6b
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-23 18:24:58 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,11 +1,13 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
+# Выбор языка
 def lang_keyboard():
     return InlineKeyboardMarkup(row_width=2).add(
         InlineKeyboardButton("🇷🇺 Русский", callback_data="lang_ru"),
         InlineKeyboardButton("🇺🇿 O‘zbekcha", callback_data="lang_uz")
     )
 
+# Главное меню
 def main_menu(lang):
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
     if lang == "ru":
@@ -16,10 +18,13 @@ def main_menu(lang):
         kb.add(KeyboardButton("⚙️ Sozlamalar"))
     return kb
 
+# Выбор отдела
 def departments_keyboard(lang):
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
     items = ["Продажи", "Технический", "Сервис"] if lang == "ru" else ["Sotuv", "Texnik", "Xizmat"]
+    back_text = "◀️ Назад" if lang == "ru" else "◀️ Orqaga"
+
     for i in items:
         kb.add(KeyboardButton(i))
-    kb.add(KeyboardButton("◀️ Назад"))
+    kb.add(KeyboardButton(back_text))
     return kb
