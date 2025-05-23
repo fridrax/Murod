5ace9ea933541acc45613f515cc3cd74aea70b15
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-23 22:36:07 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ DATABASE_URL = (
     "postgresql://sg_hotline_db_user:Edqwmk2EvU2gN6IOXTAG2jEw6NoOTRb0@dpg-d0n14t51spdvs7386kdio-a/sg_hotline_db"
 )
 
-ADMIN_CHAT_ID = 4688581564
+ADMIN_CHAT_ID = -4688581564
