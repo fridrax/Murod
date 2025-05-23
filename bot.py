fe65b985e0dc8db048c9391f898a016204ef855b
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-23 15:19:23 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
-from aiogram import executor
+import asyncio
 from loader import dp
 from database import init_db
-import handlers  # подтянет всё из handlers/__init__.py
+import handlers  # подтянет все из handlers/__init__.py
+from aiogram import executor
 
 async def on_startup(dispatcher):
     await init_db()
 
 if __name__ == "__main__":
-    executor.start_polling(dp, on_startup=on_startup)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init_db())  # инициализируем БД до запуска
+    executor.start_polling(dp, skip_updates=True)
