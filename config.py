4a4416cad6082db30c0cfadfed22410661e3680e
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-24 01:09:09 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-BOT_TOKEN = "7548380199:AAGSPBy8Ko9pfBFfSp3338U5kh1Gd_vDJW8"
+BOT_TOKEN = "7548380199:AAFdbUGaEiI3sHm9Wf7zSx5l3nl6rF_q6Eo"
 
 DATABASE_URL = (
     "postgresql://sg_hotline_db_user:Edqwmk2EvU2gN6IOXTAG2jEw6NoOTRb0@dpg-d0n14t51spdvs7386kdio-a/sg_hotline_db"
 )
 
 ADMIN_CHAT_ID = -4688581564
+postgresql://sg_hotline_db_user:EdqwmK2EvU2gN6IOXTAG2jEw6oNoTR6b@dpg-d0n14515pdvs7386kdi0-a/sg_hotline_db
