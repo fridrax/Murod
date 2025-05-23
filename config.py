ddcab5be29fe485227117371e87e65d588b4a543
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-23 22:32:46 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "7548380199:AAEcG5wdetYagtEe91_sluK-5KGqZ3VcskA"
+BOT_TOKEN = "7548380199:AAGSPBy8Ko9pfBFfSp3338U5kh1Gd_vDJW8"
 
 DATABASE_URL = (
     "postgresql://sg_hotline_db_user:Edqwmk2EvU2gN6IOXTAG2jEw6NoOTRb0@dpg-d0n14t51spdvs7386kdio-a/sg_hotline_db"
