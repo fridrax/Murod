d2e8d33db93faf398bc80d010ad888ec458b56b4
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-24 16:32:22 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -160,6 +160,7 @@ async def save_ticket(message: types.Message):
 📍 <b>Город:</b> {user_data[user_id]['city']}
 🏢 <b>Отдел:</b> {user_data[user_id]['department']}
 📝 <b>Сообщение:</b> {user_data[user_id]['message']}
+📌 <b>Статус:</b> <i>Новая</i>
 """.strip()
 
     keyboard = InlineKeyboardMarkup(row_width=2)
