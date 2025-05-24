b528857943e86bfd15be49b27f1e27f8abe22f61
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-24 15:38:56 +0500

    Delete config directory

--- config/settings.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Bot, Dispatcher
-
-# Задаём переменные вручную
-BOT_TOKEN = "7548380199:AAFrTiLUGVcN1lIQ3t2pCZ8JZlgcwAnYe0c"
-DATABASE_URL = "postgres://sg_hotline_user:EqdmK2EVuZGI0XTA6qTFJEW60NotR6dp@dbn-15l5pvs73k6diq-sg.hotline.db/sg_hotline_db"
-
-# Создаём экземпляры бота и диспетчера
-bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot)
