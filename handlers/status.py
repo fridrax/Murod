45c84e7a5fa4ad07abe1965f61ce2c337792ff7f
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-24 09:55:39 +0500

    Update status.py

--- handlers/status.py
+++ handlers/status.py
@@ -3,7 +3,7 @@ import html
 from config import DATABASE_URL
 from loader import dp
 from aiogram import types
-from utils.state import user_data  # ← перенос из handlers.start
+from utils.state import user_data
 
 @dp.message_handler(lambda m: m.text in ["📋 Статус заявки", "📊 Murojaat holati"])
 async def show_status(message: types.Message):
@@ -14,7 +14,7 @@ async def show_status(message: types.Message):
     try:
         conn = await asyncpg.connect(DATABASE_URL)
         rows = await conn.fetch(
-            "SELECT ticket_number, status FROM tickets WHERE user_id = 579130123 ORDER BY created_at DESC LIMIT 10",
+            "SELECT ticket_number, status FROM tickets WHERE user_id = $1 ORDER BY created_at DESC LIMIT 10",
             user_id
         )
     except Exception as e:
