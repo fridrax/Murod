535ee3b39b1c339e045e615192114b74869339c3
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-24 11:54:45 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-BOT_TOKEN = "7548380199:AAGeMsBBanR1g0CSp4Ee35fLh9eInk5NG9M"
-
-DATABASE_URL = (
-    "postgresql://sg_hotline_db_user:Edqwmk2EvU2gN6IOXTAG2jEw6NoOTRb0@dpg-d0n14515pdvs7386kdi0-a.oregon-postgres.render.com/sg_hotline_db"
-)
-
-ADMIN_CHAT_ID = -4688581564
