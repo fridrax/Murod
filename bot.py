8237ea635f36fc015fb5bd26d2fc64f8e83a4c9e
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-24 10:09:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,6 @@
 from aiogram import executor
 from loader import dp
-from database import init_db
 import handlers  # Импорт всех хендлеров через __init__.py
 
-# Функция запуска при старте бота
-async def on_startup(dispatcher):
-    await init_db()  # Инициализация подключения к БД
-
 if __name__ == "__main__":
-    # Стартуем бота и передаём функцию on_startup
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_polling(dp, skip_updates=True)
