df37a31a5468a4b454e784a0fd62e2f53193b4e8
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-24 15:39:26 +0500

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import start
-from . import settings
-from . import tickets
-from . import status
-from . import reply
--- handlers/settings.py
+++
@@ -1,9 +0,0 @@
-from aiogram import types
-from loader import dp
-from utils.state import user_data  # переносим из start
-
-@dp.message_handler(lambda m: m.text in ["⚙️ Настройки", "⚙️ Sozlamalar"])
-async def settings_handler(message: types.Message):
-    lang = user_data.get(message.from_user.id, {}).get("lang", "ru")
-    text = "🛠 Раздел в разработке." if lang == "ru" else "🛠 Bo‘lim ishlab chiqilmoqda."
-    await message.answer(text)
