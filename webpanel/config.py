3564fcaca709b5cd223a30abcb20837798c5d87b
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-25 14:59:50 +0500

    Update config.py

--- webpanel/config.py
+++ webpanel/config.py
@@ -1,4 +1,8 @@
-B_URI = "postgresql://sg_hotline_db_user:EdqwmK2EvU2gN6IOXTAG2jEw6oNoTR6b@dpg-d0n14515pdvs7386kdi0-a/sg_hotline_db"
-SECRET_KEY = "supersecretkey"  # можно заменить
-ADMIN_LOGIN = "admin"
-ADMIN_PASSWORD = "123456"  # поменяешь потом!
+# config.py
+
+import os
+
+DB_URI = os.environ.get("DB_URI", "postgresql://sg_hotline_db_user:EdqwmK2EvU2gN6IOXTAG2jEw6oNoTR6b@dpg-d0n14515pdvs7386kdi0-a/sg_hotline_db")
+SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
+ADMIN_LOGIN = os.environ.get("ADMIN_LOGIN", "admin")
+ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "123456")
