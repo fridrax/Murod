2b47bbc83278ebf9de338dfb94bccf2c8d4ae0c7
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-25 14:50:10 +0500

    Update config.py

--- webpanel/config.py
+++ webpanel/config.py
@@ -1,1 +1,4 @@
-
+B_URI = "postgresql://sg_hotline_db_user:EdqwmK2EvU2gN6IOXTAG2jEw6oNoTR6b@dpg-d0n14515pdvs7386kdi0-a/sg_hotline_db"
+SECRET_KEY = "supersecretkey"  # можно заменить
+ADMIN_LOGIN = "admin"
+ADMIN_PASSWORD = "123456"  # поменяешь потом!
