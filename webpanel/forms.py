eca020873d4e0bbe97f3aa5d897db3568f0390f7
fridrax <61906771+fridrax@users.noreply.github.com>
2025-05-25 14:50:45 +0500

    Update forms.py

--- webpanel/forms.py
+++ webpanel/forms.py
@@ -1,1 +1,15 @@
+# forms.py
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+class ReplyForm(FlaskForm):
+    reply = TextAreaField('Ответ', validators=[DataRequired()])
+    status = SelectField('Статус', choices=[('Новая', 'Новая'), ('В работе', 'В работе'), ('Закрыта', 'Закрыта')])
+    submit = SubmitField('Сохранить')
